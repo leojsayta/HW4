83c27263dde656bd32e3f452618d3f3c9d531838
Joel Atyas <jatyas@gmail.com>
2015-03-10 20:34:27 -0400

    Initial Commit

---
+++ HW4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW4
+//
+//  Created by Joel Atyas on 3/10/15.
+//  Copyright (c) 2015 Joel Atyas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
